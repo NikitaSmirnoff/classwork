78c36a9c8efdfc3a31260c207503490c5668baa4
Student6 <Student6@10.8.33.167>
2016-09-20 12:32:15 -0400

    Homework on Math.random

--- Introduction/src/DataTypes/DataTypes.java
+++ Introduction/src/DataTypes/DataTypes.java
@@ -3,7 +3,13 @@ package DataTypes;
 public class DataTypes {
 
 	public static void main(String[] args){
-		System.out.println(3.0+5/2+5*2); //double divided by integer
+		// System.out.println(3/2); //double divided by integer
+		
+		String ten = "10"; // Numbers are converted to strings and treated as characters, not values
+		String two = "2";
+		if (ten.compareTo(two)>0)System.out.println("10 is greater than 2");
+		else System.out.println("10 is not greater than 2"); 
+		//10 is not greater because alphabetically 2 comes after 1 and 0, thus 2 being greater
 	}
 
 }
---
+++ Introduction/src/DataTypes/MathRandom.java
@@ -0,0 +1,22 @@
+package DataTypes;
+
+import java.util.Random;
+
+public class MathRandom {
+
+	public static void main(String[] args) {
+		for(int index = 0; index < 10; index++){
+			System.out.println("Roll #" + (index + 1) + " - Die is " + rollFairDie());
+		}
+	}
+	
+	public static int rollFairDie(){
+		Random gen = new Random();
+		return gen.nextInt(6) + 1;
+	}
+	
+	public static int rollUnfairDie(){ //Roll certain numbers more often than others. UNFINISHED
+		Random gen = new Random();
+		return gen.nextInt(6) + 1;
+	}
+}

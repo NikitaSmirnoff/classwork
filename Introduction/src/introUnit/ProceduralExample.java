cf21a965a453a73374734ce87b47da44336239f6
NikitaSmirnoff <nsmirnov0027@bths.edu>
2016-09-21 09:28:48 -0400

    dfdf

--- Introduction/src/introUnit/ProceduralExample.java
+++ Introduction/src/introUnit/ProceduralExample.java
@@ -14,3 +14,24 @@ public class ProceduralExample {
 		System.out.println("My name is " + names[0] + ", and I am a " + years[0]);
 	}
 }
+
+
+
+// HOMEWORK
+
+public static void main(String[] args){
+	int[] some = {3, 4, 5};
+	System.out.println(average(some));
+}
+
+public static int average(int[] arr){
+	int sum = 0;
+	for(int i = 0, i < arr.length; i++)
+	{
+		sum += arr[i];
+	}
+	System.out.println(sum);
+	int numItems = arr.length;
+	int avg = sum / numItems;
+	return avg;
+}
